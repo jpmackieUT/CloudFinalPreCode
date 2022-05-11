694f33cb13e9cc363c8dc63582440376239a3e24
jpmackieUT <60373756+jpmackieUT@users.noreply.github.com>
2022-05-11 14:33:23 -0500

    one

---
+++ Flight3.py
@@ -0,0 +1,38 @@
+
+import sys
+import re
+import numpy as np
+
+from operator import add
+from pyspark import SparkContext
+
+# https://spark.apache.org/docs/latest/api/python/reference/pyspark.html
+# pyspark.RDD.countByKey
+# pyspark.RDD.flatMap
+# pyspark.RDD.groupBy
+# pyspark.RDD.groupByKey vs reducebykey
+# pyspark.RDD.sortBy and pyspark.RDD.sortByKey
+
+
+def combine(a, b):
+    return (a[0]+b[0], a[1]+b[1])
+
+if __name__ == "__main__":
+
+    sc = SparkContext(appName= "TaskExam")
+
+    file ="gs://luckybucky/flights-small.csv"
+
+    lines = sc.textFile(file)
+    header = lines.first()
+    lines = lines.filter(lambda x: x!= header)
+
+    lines = lines.map(lambda x: x.split(","))
+
+    lines = lines.map(lambda x: (x[0], (1, 8)))
+    lines = lines.filter(lambda x: x[1][1] != "NA")
+    lines = lines.reduceByKey(lambda a,b: (a[0]+b[0], a[1]+b[1]))
+    lines = lines.map(lambda x: (x[0], x[1][1]/x[1][0]))
+    ans = lines.top(7, key=lambda x: 10-x[1])
+    for i in ans:
+        print(i)
